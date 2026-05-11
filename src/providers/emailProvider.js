3ada7e45d5ee04905ff49163096159d06b155a11
DuongTanLoc <duongtanloc824@gmail.com>
2026-05-11 21:22:05 +0700

    feat: configure resend custom domain email sender

--- src/providers/emailProvider.js
+++ src/providers/emailProvider.js
@@ -2,23 +2,16 @@ import { Resend } from 'resend'
 import { env } from '~/config/environment'
 
 const resend = new Resend(env.RESEND_API_KEY)
-console.log('RESEND KEY:', env.RESEND_API_KEY)
 
 const sendEmail = async (to, toName, subject, html) => {
   try {
-    console.log('START SEND MAIL')
-
-    const data = await resend.emails.send({
-      from: 'onboarding@resend.dev',
+    await resend.emails.send({
+      from: 'noreply@joji.io.vn',
       to: to,
       subject,
       html
     })
-
-    console.log('MAIL SENT:', data)
-
   } catch (error) {
-    console.log('MAIL ERROR:', error)
     throw error
   }
 }
